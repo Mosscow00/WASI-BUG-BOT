77509a9d747b9a32547a4d908c7d79f30689ab82
Mosscow00 <141783259+Mosscow00@users.noreply.github.com>
2024-08-19 00:33:00 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,23 +3,23 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "201210307869"
+global.ownername = "Yy"
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '201210307869'  //creator number
+global.ownername = ' Yy ' //owner name
+global.botname = 'Yy BUG BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'WASI'
+global.author = 'Yy'
 
 //console view/theme
 global.themeemoji = '🪀'
-global.wm = "WASI TECH"
+global.wm = "Yy"
 
 //theme link
 global.link = 'https://whatsapp.com'
